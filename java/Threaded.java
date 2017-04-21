fee2686359b045709868c2289aeeff706d92b697
combineone <im@combine.one>
2017-04-22 01:37:33 +0300

    java # threaded counter with Atomic

--- java/Threaded.java
+++ java/Threaded.java
@@ -1,23 +1,13 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Threaded {
   public static void main(String[] args) throws InterruptedException {
-    class Counter {
-      private int count = 0;
-
-      public synchronized void increment() {
-        ++count;
-      }
-
-      public int getCount() {
-        return count;
-      }
-    }
-
-    final Counter counter = new Counter();
+    final AtomicInteger counter = new AtomicInteger();
 
     class CountingThread extends Thread {
       public void run() {
         for(int x = 0; x < 10000; ++x)
-          counter.increment();
+          counter.incrementAndGet();
       }
     }
 
@@ -30,6 +20,6 @@ public class Threaded {
     thread1.join();
     thread2.join();
 
-    System.out.println(counter.getCount());
+    System.out.println(counter.get());
   }
 }
